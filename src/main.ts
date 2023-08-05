c2120c24262437aea84aa36026702b94dbef1d36
Heshan <heshan.ua@gmail.com>
2023-08-05 20:28:22 +0530

    Adding sessions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe} from "@nestjs/common"
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe())
+
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe, Request, Session, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
@@ -24,7 +24,28 @@ export class UserController {
     }
 
     @Get('/admin')
-    getUsersAdmin() {
+    getUsersAdmin(@Session() session: any, @Request() req: any) {
+        console.log('request.session', req.session);
+
+        if (!session.user) {
+            throw new UnauthorizedException();
+        }
+
         return this.userService.getUserById(1);
+    }
+
+    @Post('login')
+    login(@Session() session: any) {
+        session.user = { id: 123, name: 'appUser' };
+    }
+
+    @Get('auth-user')
+    getSession(@Session() session: any) {
+        return session.user;
+    }
+
+    @Post('logout')
+    logout(@Session() session: any) {
+        session.user = null;
     }
 }
